2856c1a9761e6a24a22b9028797f99a15a56cfdc
kyoheitsuno <freetrip555@gmail.com>
2019-12-08 19:09:23 +0900

    リスト6-21まで

--- app/Person.php
+++ app/Person.php
@@ -4,32 +4,21 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use App\Scopes\ScopePerson;
 
 class Person extends Model
 {
-    protected static function boot(){
-        parent::boot();
-    
-        static::addGlobalScope('age', function (Builder $builder) {
-            $builder->where('age', '>', 20);
-        });
-    }
+   protected $guarded = array('id');
 
-    public function getData(){
-        return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
-    }
+   public static $rules = array(
+      'name' => 'required',
+      'mail' => 'email',
+      'age' => 'integer|min:0|max:150'
+   );
 
-    public function scopeNameEqual($query, $str) {
-        return $query->where('name', $str);
-    }
-
-    public function scopeAgeGreaterThan($query, $n) {
-        return $query->where('age','>=', $n);
-    }
-
-    public function scopeAgeLessThan($query, $n) {
-        return $query->where('age', '<=', $n);
-    }
+   // getDataは残しておく
+   public function getData()
+   {
+      return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
+   }
 }
-
-
