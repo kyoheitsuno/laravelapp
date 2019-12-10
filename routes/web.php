2aeccd1762396c5ada6599efd7caac6627f37eca
kyoheitsuno <freetrip555@gmail.com>
2019-12-10 16:04:13 +0900

    リスト7-7

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,4 @@ Route::get('board', 'BoardController@index');
 
 Route::get('board/add', 'BoardController@add');
 Route::post('board/add', 'BoardController@create');
+Route::resource('rest', 'RestappController');
